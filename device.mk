72d2ca645329e1a1f50ad1ea51afa4314c08fbc6
pexcn <pexcn97@gmail.com>
2025-07-21 11:01:31 +0800

    giulia: overlay: Set hostapd default ssid for CN variant

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
     OPlusSettingsProviderResTargetCN \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget \
-    OPlusWifiResTarget
+    OPlusWifiResTarget \
+    OPlusWifiResTargetCN
 
 # Regional properties
 PRODUCT_COPY_FILES += \
