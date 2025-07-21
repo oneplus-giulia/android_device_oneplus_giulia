1d43c722e433f69abd721664a09b55db99edfa2a
pexcn <pexcn97@gmail.com>
2025-07-21 10:55:45 +0800

    giulia: overlay: Set default device name for CN variant

--- device.mk
+++ device.mk
@@ -40,6 +40,7 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusFrameworksResTargetCN \
     OPlusSettingsProviderResTarget \
+    OPlusSettingsProviderResTargetCN \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget \
     OPlusWifiResTarget
